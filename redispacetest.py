ee6f1c9e7e983b8894681cdfa67c0b39c1274bf2
Hyunjun Kim <kim@hyunjun.kr>
2012-11-01 14:30:32 +0900

    update test

--- redispacetest.py
+++ redispacetest.py
@@ -1,24 +1,26 @@
-from attest import Tests
+from attest import TestBase, Tests, assert_hook, test
+from redispace import *
 
 
-tests = Tests()
+class RedisBlockTest(TestBase):
+   
+    @test
+    def get_node_by_rotation(self):
+        nodes = [RedisNode(), RedisNode(), RedisNode()]
+        rb = RedisBlock(nodes)
+        assert rb.get_node_by_rotation() != rb.get_node_by_rotation()
+        got_nodes = [
+            rb.get_node_by_rotation(),
+            rb.get_node_by_rotation(),
+            rb.get_node_by_rotation(),
+            rb.get_node_by_rotation(),
+            rb.get_node_by_rotation(),
+        ]
+        assert got_nodes[0] == got_nodes[3]
 
 
-@tests.test
-def redisblock():
-    from redispace import RedisBlock, RedisNode
-    nodes = [RedisNode(), RedisNode(), RedisNode()]
-    rb = RedisBlock(nodes)
-    assert rb.get_node_by_rotation() != rb.get_node_by_rotation()
-    got_nodes = [
-        rb.get_node_by_rotation(),
-        rb.get_node_by_rotation(),
-        rb.get_node_by_rotation(),
-        rb.get_node_by_rotation(),
-        rb.get_node_by_rotation(),
-    ]
-    assert got_nodes[0] == got_nodes[3]
+suite = Tests([RedisBlockTest()])
 
 
 if __name__ == '__main__':
-    tests.run()
+    suite.run()

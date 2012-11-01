d2b97ab155c0ffcc400680a8c1fd6aa5d30b1d0f
Hyunjun Kim <kim@hyunjun.kr>
2012-11-01 13:42:43 +0900

    RedisBlock rotation test

--- redispacetest.py
+++ redispacetest.py
@@ -5,8 +5,19 @@ tests = Tests()
 
 
 @tests.test
-def dummy():
-    assert True == True
+def redisblock():
+    from redispace import RedisBlock, RedisNode
+    nodes = [RedisNode(), RedisNode(), RedisNode()]
+    rb = RedisBlock(nodes)
+    assert rb._get_node_by_rotation() != rb._get_node_by_rotation()
+    got_nodes = [
+        rb._get_node_by_rotation(),
+        rb._get_node_by_rotation(),
+        rb._get_node_by_rotation(),
+        rb._get_node_by_rotation(),
+        rb._get_node_by_rotation(),
+    ]
+    assert got_nodes[0] == got_nodes[3]
 
 
 if __name__ == '__main__':
